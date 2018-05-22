8969ad6ba793602ea89491e6528ae1cbb8c77621
sa-well-ev <sa-well-ev@yandex.ru>
2018-05-22 23:31:19 +0700

    Generate User authentication files
    Generate using Gii necessary files for authentication of users

---
+++ models/user/UserRecord.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models\user;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property string $auth_key
+ */
+class UserRecord extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password'], 'required'],
+            [['username', 'password', 'auth_key'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'auth_key' => 'Auth Key',
+        ];
+    }
+}
