342d991361a8cece4dd9745b8445c2f0d98de60a
Дмитрий <sa-well-ev@yandex.ru>
2018-06-08 16:43:09 +0700

    Dropdown login form: using PJAX
    only added two rows PJAX::begin() PJAX::end()

--- views/main/login.php
+++ views/main/login.php
@@ -7,11 +7,16 @@
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\widgets\Pjax;
 
+Pjax::begin();
 $form = ActiveForm::begin([
     'action' => Url::to(['main/login']),
     'id' => 'login-form',
-    'options' => ['style' => 'padding: 0px 15px'],
+    'options' => [
+        'style' => 'padding: 0px 15px',
+        'data' => ['pjax' => true]
+    ],
     'fieldConfig' => [
         'labelOptions' => ['style' => 'white-space: nowrap'],
     ],
@@ -21,3 +26,4 @@ echo $form->field($model, 'password')->passwordInput();
 echo $form->field($model, 'rememberMe')->checkbox(['labelOptions' => ['style' => 'white-space: nowrap']]);
 echo Html::submitButton('Войти', ['class' => 'btn btn-primary', 'name' => 'login-button']);
 ActiveForm::end();
+Pjax::end();
