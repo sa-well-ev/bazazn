00eabc58ef7d9355d507abbd43282a48a64aeb0c
Дмитрий <sa-well-ev@yandex.ru>
2018-06-20 10:13:59 +0700

    Parsing the filename (commit for exchange between workplaces)
    Created a function that converts data with russian month to date format DD.MM.YYYY.

--- models/file/FileUpload.php
+++ models/file/FileUpload.php
@@ -67,13 +67,41 @@ class FileUpload extends Model
 
         preg_match('/от\s+(\d{1,2}\s+\w+\s+\d{2,4}|\d{1,2}[\._-]?\d{1,2}[\._-]?\d{2,4})/iu', $nameFile ,$m); //Просто любая дата [1]
         $matches['Дата'] = isset($m[1]) ? preg_replace('/[_-]/', '.', $m[1]) : null; // TODO: преобразовать в Timestamp
+        $matches['Дата'] = self::getFromRusDate($matches['Дата']);
         preg_match('/\(ред\.\s+(\d{1,2}\s+\w+\s+\d{2,4}|\d{1,2}[\._-]?\d{1,2}[\._-]?\d{2,4})/iu', $nameFile ,$m); //Просто любая дата [1]
         $matches['Редакция'] = isset($m[1]) ? preg_replace('/[_-]/', '.', $m[1]) : null; // TODO: преобразовать в Timestamp или класс DateTime($m[1]) DateTime::createFromFormat('d-m-y', $input)
+        $matches['Редакция'] = self::getFromRusDate($matches['Редакция']);
         preg_match('/(?<!делу|дело)\s+(№|N)\s+([^\s\.]+)/iu', $nameFile ,$m); //Номер документа - перовое слово между пробелами после номера [2]
         $matches['Номер'] = isset($m[2]) ? preg_replace('/_/', '/', $m[2]) : null;
 //        $matches['Номер'] = isset($m) ? $m : null;
         preg_match('/(по\s+делу|дело)\s+(№|N)\s+([^\s\.]+)/iu', $nameFile ,$m); //Номер документа - перовое слово между пробелами после слов по делу номера [3]
         $matches['Номер дела'] = isset($m[3]) ? preg_replace('/_/', '/', $m[3]) : null;
         return $matches;
+    }
+    /**
+     * Функция преобразовывающая дату содержащую русские месяцы в дату формата ДД.ММ.ГГГГ
+     * @param string $dateString входящая строка содержащая дату с русскими месяцами
+     * @property array $rusMonths справочник слов месяцев, которые подлежат замене
+     * @property array $numMonths справочник номеров месяцев, на которые заменяются слова месяцев
+     * @return string преобразованная строка даты в формате ДД.ММ.ГГГГ
+     */
+    public static function getFromRusDate($dateString)
+    {
+        $rusMonths = [
+            '/\s+(январь|января)\s+/iu',
+            '/\s+(февраль|февраля)\s+/iu',
+            '/\s+(март|марта)\s+/iu',
+            '/\s+(апрель|апреля)\s+/iu',
+            '/\s+(май|мая)\s+/iu',
+            '/\s+(июнь|июня)\s+/iu',
+            '/\s+(июль|июля)\s+/iu',
+            '/\s+(август|августа)\s+/iu',
+            '/\s+(сентябрь|сентября)\s+/iu',
+            '/\s+(октябрь|октября)\s+/iu',
+            '/\s+(ноябрь|ноября)\s+/iu',
+            '/\s+(декабрь|декабря)\s+/iu'
+        ];
+        $numMonths = ['.01.', '.02.', '.03.', '.04.', '.05.', '.06.', '.07.', '.08.', '.09.', '.10.', '.11.', '.12.'];
+        return preg_replace($rusMonths, $numMonths, $dateString);
     }
 }
